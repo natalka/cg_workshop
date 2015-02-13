4e0db0ceb22038d0b79de3901c90b2eed541eecf
Natalia Anna Tyszkiewicz <tyszkiewicz@Natalias-MacBook-Air.local>
2015-02-13 18:52:35 +0100

    adjust Product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,17 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, format: { :with => /\A\d+(?:\.\d{0,2})?\z/ }
+
+  def average_rating
+    if reviews && reviews.map(&:rating)
+      list = reviews.map(&:rating)
+      return list.sum.to_f / list.size
+    end
+    0
+  end
+
 end
